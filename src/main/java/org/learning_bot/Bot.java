3a10618c12274081497956512ef23a7b514b1ba6
Konrad <127674711+konradoCh@users.noreply.github.com>
2025-03-05 19:31:45 +0100

    Update Bot.java
    added username

--- src/main/java/org/learning_bot/Bot.java
+++ src/main/java/org/learning_bot/Bot.java
@@ -2,6 +2,6 @@ package org.learning_bot;
 
 public class Bot {
     static final String BOT_TOKEN= "7562844377:AAHXSIq68JillzNVXS1O_GILSwK-SnPvL_I";
-    static final String USERNAME = "";
+    static final String USERNAME = "learning_90_bot";
 
 }
