f1ceac821d084735f3addc2bc3332057834c9898
kompu <kompu@oo.com>
2025-03-06 09:32:40 +0100

    commit

---
+++ src/main/java/org/learning_bot/Main.java
@@ -0,0 +1,19 @@
+package org.learning_bot;
+
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try{
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            Responder responder = new Responder();
+            telegramBotsApi.registerBot(responder);
+        } catch (TelegramApiException telegramApiException) {
+            telegramApiException.printStackTrace();
+        }
+    }
+}
--- src/main/java/org/learning_bot/Responder.java
+++ src/main/java/org/learning_bot/Responder.java
@@ -1,12 +1,41 @@
 package org.learning_bot;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class Responder extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
-        System.out.println(update.getMessage().getText());
+
+        String response = "I am sorry, I don't understand the message you sent";
+
+        String chatId = update.getMessage().getChatId().toString();
+
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(response);
+
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String userMessage = update.getMessage().getText().trim();
+
+            if (userMessage.equalsIgnoreCase("Hello")) {
+                sendMessage.setText("How are you?");
+            }
+
+            if (userMessage.equalsIgnoreCase("How are you?")) {
+                sendMessage.setText("I'm fine thank you");
+            }
+        }
+
+
+        try {
+            sendApiMethod(sendMessage);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     @Override
